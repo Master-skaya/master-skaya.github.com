331e08e2fe07a12cd1ec9057bc70d4bdad04ed58
Timur Taipov <taipov@gmail.com>
2015-08-15 19:12:12 +0500

    сообщения о завявке

--- js/regform.js
+++ js/regform.js
@@ -11,15 +11,15 @@ $contactForm.submit(function(e) {
         data: $(this).serialize(),
         dataType: 'json',
         beforeSend: function() {
-            $contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+            $contactForm.append('<div class="alert alert--loading">Данные отправляются…</div>');
         },
         success: function(data) {
             $contactForm.find('.alert--loading').hide();
-            $contactForm.append('<div class="alert alert--success">Message sent!</div>');
+            $contactForm.append('<div class="alert alert--success">Заявка зарегистрирована!</div>');
         },
         error: function(err) {
             $contactForm.find('.alert--loading').hide();
-            $contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+            $contactForm.append('<div class="alert alert--error">Ошибка.</div>');
         }
     });
 });
